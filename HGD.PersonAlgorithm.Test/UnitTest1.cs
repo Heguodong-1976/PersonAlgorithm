d83cbe6e1a922a2dcdffb1bdad536717b6f8dae3
guodong he <32354456@qq.com>
2017-11-03 10:22:43 +0800

    add an empty test class to verify git operation

--- HGD.PersonAlgorithm.Test/UnitTest1.cs
+++ HGD.PersonAlgorithm.Test/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace HGD.PersonAlgorithm.Test
         [TestMethod]
         public void TestMethod1()
         {
-            var i = Class1.Hello();
+            var i = Splitter.Hello();
         }
     }
 }
---
+++ HGD.PersonAlgorithm/Splitter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGD.PersonAlgorithm
+{
+    public class Splitter
+    {
+        /// <summary>
+        /// Split collection to n
+        /// </summary>
+        /// <param name="collection">Spliting collection</param>
+        /// <param name="n">Splitting count</param>
+        /// <returns>n array with same length</returns>
+        public static IEnumerable<T[]> To_n_Parts<T>(IEnumerable<T> collection,Int32 n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
